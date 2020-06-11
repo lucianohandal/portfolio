f0d1ca6ee740cdc56647e3feaae19c76fbc8cc66
lhandalb <lhandalb@github.com>
2020-06-11 00:15:32 -0400

    initial commit

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_view():
+		return render_template('portfolio.html')
+
+# @app.errorhandler(404)
+# def not_found(e):
+#   return render_template("error.html", error_code=404)
