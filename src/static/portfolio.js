420f470fb2e2a61065f0ca8a21b40e7dac1bb62a
lhandalb <lhandalb@github.com>
2020-06-12 18:38:50 -0400

    Added contact + navbar

--- src/static/portfolio.js
+++ src/static/portfolio.js
@@ -1,5 +1,69 @@
-const hello_h1 = $("#hello_world")[0]
-const hello_msg = "Hello World!"
+const navbar = $("#navbar")[0];
+const nav_items = $('.nav-item');
+const hello_h1 = $("#hello_world")[0];
+const hello_msg = "Hello World!";
+const url = document.URL;
+const modal_msg = "Do you want to include the section to the link?";
+
+var current_section = 'hello';
+var current_color = '#222831';
+var sectionOffsets;
+var sectNum = {};
+
+function getSectionsOffsets(){
+  sectionOffsets = [];
+  let offset;
+  let color;
+  let id;
+  let i = 0;
+  for (section of $('.section')) {
+    offset = section.offsetTop + section.offsetHeight;
+    color = $(section).css("background-color");
+    id = section.id;
+    sectionOffsets.push({"offset":offset, "bg_color":color, "id":id});
+    sectNum[id] = i++;
+  }
+}
+getSectionsOffsets();
+
+function showPosition(i) {
+  if (current_section == sectionOffsets[i+1].id) {
+    return;
+  }
+  current_section = sectionOffsets[i+1].id;
+  current_color = sectionOffsets[i+1].bg_color;
+  if (i < 0) {
+    $('.active').removeClass('active');
+    return;
+  }
+  $("#navbar").show();
+  $('.active').removeClass('active');
+  $(nav_items[i]).addClass('active');
+}
+
+function colorCoordinate() {
+  $(navbar).css('background-color', current_color)
+  $('.modal-content').css('background-color', current_color)
+}
+
+function scrollNavbar(){
+  let scrollPosition = $(window).scrollTop();
+  for (var i = 0; i < sectionOffsets.length; i++) {
+    if (scrollPosition < sectionOffsets[i].offset) {
+      showPosition(i - 1)
+      colorCoordinate();
+      break
+    }
+  }
+}
+
+function goToSection(section_id){
+  if (sectNum[section_id] != null) {
+    window.scrollTo(0, sectionOffsets[sectNum[section_id] - 1].offset);
+  }
+}
+
+
 
 function typeChar(i) {
     hello_h1.innerText = hello_msg.substring(0, i) + '|';
@@ -15,14 +79,46 @@ function typeHello() {
         }, hello_msg.length * time);
 }
 
-function blinkLine() {
-    const time = 50;
-    let i = 0
-    while (false){
-        setTimeout(function(){$("#type_pos").css("color", "#32e0c4")}, i++ * time);
-        setTimeout(function(){$("#type_pos").css("color", "#222831")}, i * time);
-    }
+function directLink() {
+  let query = url.split('?',2)[1];
+  if (query == null || query =="") {
+    return;
+  }
+  goToSection(query);
 }
 
-// typeHello();
-// blinkLine();
+function copyToClipboard(str) {
+  console.log(str);
+  const tempInput = document.createElement("input");
+  tempInput.value = str;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  tempInput.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
+}
+
+function shareCleanUp() {
+  $('.modal-body').show();
+  $('.modal_btn').show();
+  $('#dismiss_btn').hide();
+  $('#share_modal_title')[0].textContent = modal_msg;
+}
+
+function share(include_section){
+  let share_link = url.split('?',2)[0];
+  if (include_section && current_section != 'hello') {
+    share_link += '?' + current_section;
+  }
+  copyToClipboard(share_link);
+  $('.modal-body').hide();
+  $('.modal_btn').hide();
+  $('#dismiss_btn').show();
+  $('#share_modal_title')[0].textContent = "Link Copied";
+}
+
+window.onscroll = function() {scrollNavbar()};
+typeHello();
+$( document ).ready(function() {
+    directLink();
+});
